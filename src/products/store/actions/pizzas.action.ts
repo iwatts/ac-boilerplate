af87263c993d586aeb84c481abef73bbade498ad
Isaac <isaac.r.watts@gmail.com>
2018-10-16 17:02:26 -0600

    bp init exercise

--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -10,4 +10,22 @@ export class MyAction implements Action {
 }
 
 // action types
-export type PizzasAction = MyAction;
+export const LOAD_PIZZAS = '[Pizzas] Load';
+export const LOAD_PIZZAS_FAIL = '[Pizzas] Load Fail';
+export const LOAD_PIZZAS_SUCCESS = '[Pizzas] Load Success';
+
+export class LoadPizzas implements Action {
+    readonly type = LOAD_PIZZAS;
+    constructor(public payload: any) {}
+}
+export class LoadPizzasFail implements Action {
+    readonly type = LOAD_PIZZAS_FAIL;
+    constructor(public payload: any) {}
+}
+export class LoadPizzasSuccess implements Action {
+    readonly type = LOAD_PIZZAS_SUCCESS;
+    constructor(public payload: Pizza[]) {}
+}
+
+
+export type PizzasAction = LoadPizzas | LoadPizzasFail | LoadPizzasSuccess;
